e4a58823b2507cc96ada3ed77d0d259f9a924961
Alessandro Molinari <alessandromolinari2000@hotmail.com>
2021-06-28 17:05:17 +0000

    updated with easier solution

--- main.py
+++ main.py
@@ -4,23 +4,33 @@ from replit import clear
 auction_pool = {}
 end_of_bids = False
 
+def find_highest_bidder(bidding_record):
+  highest_bid = 0
+  for bidder in bidding_record:
+    bid_amount = bidding_record[bidder]
+    if bid_amount > highest_bid:
+      highest_bid = bid_amount
+      winner = bidder
+  print(f'the winner is {winner}, with a bid of {highest_bid}')
+    
+
 
 from art import logo
-import operator
+
 
 print(logo)
 while not end_of_bids:
   
   name = input("Please insert your name: ")
   bid = input("Please insert your bid: ")
-  auction_pool[name] = bid
+  auction_pool[name] = int(bid)
   more_players = input('Are there more players? (y/n): ') 
   
   if more_players == 'y':
     clear()
   elif more_players == 'n': 
-    highest_bidder = max(auction_pool.items(), key=operator.itemgetter(1))[0]
-    print(f'The winner of the auction is {highest_bidder} with a bid of {auction_pool[highest_bidder]}')
+    find_highest_bidder(auction_pool)
+
 
     end_of_bids = True
 
@@ -30,7 +40,10 @@ while not end_of_bids:
 
  
 
-
+'''
+    highest_bidder = max(auction_pool.items(), key=operator.itemgetter(1))[0]
+    print(f'The winner of the auction is {highest_bidder} with a bid of {auction_pool[highest_bidder]}')
+    '''
 
 
 
